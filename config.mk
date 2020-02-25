3c01e18f11a25f74e08ef9c9b3b99a0233510833
hariimurti <hariimurti@gmail.com>
2020-02-25 23:40:39 +0700

    vendor: initial commit

---
+++ config.mk
@@ -0,0 +1,13 @@
+# define build type
+HAVOC_BUILD_TYPE := Custom
+
+# product app
+PRODUCT_PACKAGES += \
+    MiCalculator \
+    MiRemote \
+    ViaBrowser
+
+# product priv-app
+PRODUCT_PACKAGES += \
+    ScreenRecordStubPrebuilt
+
